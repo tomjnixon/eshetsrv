fffb9b244dc667ed3a1f4817a007598062fd3f42
Thomas Nixon <tom@tomn.co.uk>
2020-04-05 16:09:33 +0100

    add port configuration options

--- apps/eshethttp/src/eshethttp_app.erl
+++ apps/eshethttp/src/eshethttp_app.erl
@@ -6,12 +6,14 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+    Port = application:get_env(eshethttp, port, 11237),
+
     Dispatch = cowboy_router:compile([{'_',
                                        [{"/call/[...]",
                                          eshethttp_action_call,
                                          [eshetsrv_state]}]}]),
     {ok, _} = cowboy:start_clear(eshethttp,
-                                 [{port, 8080}],
+                                 [{port, Port}],
                                  #{env => #{dispatch => Dispatch}}),
     eshethttp_sup:start_link().
 
